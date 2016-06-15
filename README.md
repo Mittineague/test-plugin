d2803823aca2853246792d80a81bfc1324eabe84
Mittineague <allanh62@hotmail.com>
2016-06-15 17:42:20 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# test-plugin
+
+This plugin is only a **very crude, very alpha** and transitory plugin.  
+It is for my own testing purposes only and in no way intended to be used in any way by others.  
+###There is a better than good chance that installing it will break your Discourse.  
+
+
+If and when the code proves to be safe to use, it will be moved into a proper plugin. 
+
+## Installation
+
+I **strongly"" advise you to _not_ install this plugin. 
+
+## Authors
+
+Mittineague
+
+## License
+
+GNU GPL v2 
+
+## Support 
+
+I'm still learning so you are most likely a better coder than I am, but I frequently visit these forums and either I or others should be able to help.  
+[SitePoint](https://www.sitepoint.com/community/)  
+[Discourse Meta](https://meta.discourse.org/)  
---
+++ plugin.rb
@@ -0,0 +1,34 @@
+# name: test-plugin
+# about: testing repository transitory plugin
+# version: 0.1
+# authors: Mittineague
+# url: https://github.com/Mittineague/test-plugin.git
+
+
+after_initialize do
+
+  class Guardian
+  
+    def can_send_private_message?(target)
+      (target.is_a?(Group) || target.is_a?(User)) &&
+      # User is authenticated
+      authenticated? &&
+      # Can't send message to yourself
+      is_not_me?(target) &&
+      # Have to be a basic level at least
+# attempt to make Guardian kinder to New members
+      #@user.has_trust_level?(SiteSetting.min_trust_to_send_messages) &&
+      (@user.has_trust_level?(SiteSetting.min_trust_to_send_messages) || target.staff?) &&
+      # PMs are enabled
+      (SiteSetting.enable_private_messages ||
+        @user.username == SiteSetting.site_contact_username ||
+        @user == Discourse.system_user) &&
+      # Can't send PMs to suspended users
+      (is_staff? || target.is_a?(Group) || !target.suspended?) &&
+      # Blocked users can only send PM to staff
+      (!@user.blocked? || target.staff?)
+    end
+
+  end
+  
+end  
