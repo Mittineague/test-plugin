b9591d1d7f295f5bcc082fe9b396b59199f408f1
Mittineague <allanh62@hotmail.com>
2016-06-15 19:43:34 -0400

    attempt five

--- plugin.rb
+++ plugin.rb
@@ -4,33 +4,28 @@
 # authors: Mittineague
 # url: https://github.com/Mittineague/test-plugin.git
 
+class Guardian
+  class << self
 
-after_initialize do
-
-  class Guardian
-    class << self
-
-      def can_send_private_message?(target)
-        (target.is_a?(Group) || target.is_a?(User)) &&
-        # User is authenticated
-        authenticated? &&
-        # Can't send message to yourself
-        is_not_me?(target) &&
-        # Have to be a basic level at least
+    def can_send_private_message?(target)
+      (target.is_a?(Group) || target.is_a?(User)) &&
+      # User is authenticated
+      authenticated? &&
+      # Can't send message to yourself
+      is_not_me?(target) &&
+      # Have to be a basic level at least
 # attempt to make Guardian kinder to New members
-        #@user.has_trust_level?(SiteSetting.min_trust_to_send_messages) &&
-        (@user.has_trust_level?(SiteSetting.min_trust_to_send_messages) || target.staff?) &&
-        # PMs are enabled
-        (SiteSetting.enable_private_messages ||
-          @user.username == SiteSetting.site_contact_username ||
-          @user == Discourse.system_user) &&
-        # Can't send PMs to suspended users
-        (is_staff? || target.is_a?(Group) || !target.suspended?) &&
-        # Blocked users can only send PM to staff
-        (!@user.blocked? || target.staff?)
-      end
-
+      #@user.has_trust_level?(SiteSetting.min_trust_to_send_messages) &&
+      (@user.has_trust_level?(SiteSetting.min_trust_to_send_messages) || target.staff?) &&
+      # PMs are enabled
+      (SiteSetting.enable_private_messages ||
+        @user.username == SiteSetting.site_contact_username ||
+        @user == Discourse.system_user) &&
+      # Can't send PMs to suspended users
+      (is_staff? || target.is_a?(Group) || !target.suspended?) &&
+      # Blocked users can only send PM to staff
+      (!@user.blocked? || target.staff?)
     end
+
   end
-  
-end  
+end
