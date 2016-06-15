1dce3123d9fdcb4ff38dd7e6faed9601a16908b4
Mittineague <allanh62@hotmail.com>
2016-06-15 18:33:27 -0400

    attempt two

--- plugin.rb
+++ plugin.rb
@@ -8,25 +8,29 @@
 after_initialize do
 
   class Guardian
-  
-    def can_send_private_message?(target)
-      (target.is_a?(Group) || target.is_a?(User)) &&
-      # User is authenticated
-      authenticated? &&
-      # Can't send message to yourself
-      is_not_me?(target) &&
-      # Have to be a basic level at least
+    class << self
+      alias_method :kinder_guardian?, :can_send_private_message?
+
+      def can_send_private_message?(target)
+        (target.is_a?(Group) || target.is_a?(User)) &&
+        # User is authenticated
+        authenticated? &&
+        # Can't send message to yourself
+        is_not_me?(target) &&
+        # Have to be a basic level at least
 # attempt to make Guardian kinder to New members
-      #@user.has_trust_level?(SiteSetting.min_trust_to_send_messages) &&
-      (@user.has_trust_level?(SiteSetting.min_trust_to_send_messages) || target.staff?) &&
-      # PMs are enabled
-      (SiteSetting.enable_private_messages ||
-        @user.username == SiteSetting.site_contact_username ||
-        @user == Discourse.system_user) &&
-      # Can't send PMs to suspended users
-      (is_staff? || target.is_a?(Group) || !target.suspended?) &&
-      # Blocked users can only send PM to staff
-      (!@user.blocked? || target.staff?)
+        #@user.has_trust_level?(SiteSetting.min_trust_to_send_messages) &&
+        (@user.has_trust_level?(SiteSetting.min_trust_to_send_messages) || target.staff?) &&
+        # PMs are enabled
+        (SiteSetting.enable_private_messages ||
+          @user.username == SiteSetting.site_contact_username ||
+          @user == Discourse.system_user) &&
+        # Can't send PMs to suspended users
+        (is_staff? || target.is_a?(Group) || !target.suspended?) &&
+        # Blocked users can only send PM to staff
+        (!@user.blocked? || target.staff?)
+        kinder_guardian?(target)
+      end
     end
 
   end
