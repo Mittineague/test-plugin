80e5deadaadb97224b9e1ad14699bf4c0e997bb2
Mittineague <allanh62@hotmail.com>
2016-12-25 22:57:51 -0500

    likes count test take two

--- plugin.rb
+++ plugin.rb
@@ -11,23 +11,22 @@ after_initialize_do
     class Engine < ::Rails::Engine
       engine_name "test_plugin"
       isolate_namespace TestPlugin
-    end
+  end
 
-    require_dependency 'user'
-    def current_user_id
-      user.current_user.id
-    end
+  require_dependency 'user'
+  def current_user_id
+    user.current_user.id
+  end
 
-    def user_like_count
-#      User.like_oount(current_user)
-      sql = <<-SQL
+  def user_like_count
+#   User.like_oount(current_user)
+    sql = <<-SQL
             SELECT user.like_count
             FROM user
             WHERE user.id = :current_user_id
-      SQL
-      ActiveRecord::Base.exec_sql(sql)
+    SQL
+    ActiveRecord::Base.exec_sql(sql) rescue nil
 #      render json: TestPlugin.user_like_count
-    end
   end
 end
 
