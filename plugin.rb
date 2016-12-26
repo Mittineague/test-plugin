efc354ab2554436ffebaa86514bcca73844dfc44
Mittineague <allanh62@hotmail.com>
2016-12-25 22:25:01 -0500

    likes count test take one

--- plugin.rb
+++ plugin.rb
@@ -4,42 +4,31 @@
 # authors: Mittineague
 # url: https://github.com/Mittineague/test-plugin.git
 
-require 'topic_user'
+register_asset "javascripts/discourse/templates/connectors/user-card-post-names/mitt-test-plugin.hbs"
 
-module TopicUserFix
-  def self.included base
-    base.class_eval do
+after_initialize_do
+  module ::TestPlugin
+    class Engine < ::Rails::Engine
+      engine_name "test_plugin"
+      isolate_namespace TestPlugin
+    end
 
-    self.(:remove_const, UPDATE_TOPIC_USER_SQL) if const_defined?(UPDATE_TOPIC_USER_SQL)
-    const_set(UPDATE_TOPIC_USER_SQL, "UPDATE topic_users
-                                    SET
-                                      last_read_post_number = GREATEST(:post_number, tu.last_read_post_number),
-                                      highest_seen_post_number = t.highest_post_number,
-                                      total_msecs_viewed = LEAST(tu.total_msecs_viewed + :msecs,86400000),
-                                      notification_level =
-                                         case when tu.notifications_reason_id is null and (tu.total_msecs_viewed + :msecs) >
-                                            coalesce(uo.auto_track_topics_after_msecs,:threshold) and
-                                            coalesce(uo.auto_track_topics_after_msecs, :threshold) >= 0 then
-                                              :tracking
-                                         else
-                                            tu.notification_level
-                                         end
-                                      first_visited_at = COALESCE(tu.first_visited_at, DateTime.now)
-                                  FROM topic_users tu
-                                  join topics t on t.id = tu.topic_id
-                                  join users u on u.id = :user_id
-                                  join user_options uo on uo.user_id = :user_id
-                                  WHERE
-                                       tu.topic_id = topic_users.topic_id AND
-                                       tu.user_id = topic_users.user_id AND
-                                       tu.topic_id = :topic_id AND
-                                       tu.user_id = :user_id
-                                  RETURNING
-                                    topic_users.notification_level, tu.notification_level old_level, tu.last_read_post_number
-                                ")
+    require_dependency 'user'
+    def current_user_id
+      user.current_user.id
+    end
 
-   end
- end
+    def user_like_count
+#      User.like_oount(current_user)
+      sql = <<-SQL
+            SELECT user.like_count
+            FROM user
+            WHERE user.id = :current_user_id
+      SQL
+      ActiveRecord::Base.exec_sql(sql)
+#      render json: TestPlugin.user_like_count
+    end
+  end
 end
 
-TopicUser.send(:include, TopicUserFix)
+# TopicUser.send(:include, TopicUserFix)
