d91cc45f3e6595cc56994ee70829c6ddbb4325b8
Mittineague <allanh62@hotmail.com>
2016-12-26 00:40:11 -0500

    likes count test take eight

--- plugin.rb
+++ plugin.rb
@@ -4,6 +4,8 @@
 # authors: Mittineague
 # url: https://github.com/Mittineague/test-plugin.git
 
+enabled_site_setting :test_plugin_enabled
+
 register_asset "javascripts/discourse/templates/connectors/user-card-post-names/mitt-test-plugin.hbs"
 
 after_initialize do
