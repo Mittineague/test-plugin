c5a301ebc8a5074cb1e8ee7ecd39ae305d4ffc56
Mittineague <allanh62@hotmail.com>
2016-12-27 01:54:31 -0500

    likes count test take thirty

--- connectors/user-card-post-names/test-plugin.js.es6
+++ connectors/user-card-post-names/test-plugin.js.es6
@@ -1,5 +1,11 @@
 export default {
   setupComponent(args, component) {
     component.set('user_like_count', '99');
-  }
+  };
+
+api.registerConnectorClass('user-card-post-names', 'test-plugin', {
+    shouldRender(args, component) {
+        return component.siteSettings.test_plugin_enabled;
+    }
+});
 }
--- plugin.rb
+++ plugin.rb
@@ -9,7 +9,7 @@ enabled_site_setting :test_plugin_enabled
 # register_asset "javascripts/discourse/controllers/test-plugin.js.es6"
 # register_asset "javascripts/discourse/helpers/test-plugin.js.es6"
 register_asset "javascripts/discourse/initializers/test-plugin.js.es6"
-register_asset "javascripts/discourse/templates/connectors/user-card-post-names/mitt-test-plugin.hbs"
+register_asset "javascripts/discourse/templates/connectors/user-card-post-names/test-plugin.hbs"
 
 PLUGIN_NAME ||= 'test_plugin'.freeze
 
