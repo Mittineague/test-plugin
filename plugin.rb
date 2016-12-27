c2358f8c1c553f899b8814dea78a2b800e134354
Mittineague <allanh62@hotmail.com>
2016-12-27 03:18:32 -0500

    likes count test take thirty-three

--- plugin.rb
+++ plugin.rb
@@ -9,7 +9,7 @@ enabled_site_setting :test_plugin_enabled
 # register_asset "javascripts/discourse/controllers/test-plugin.js.es6"
 # register_asset "javascripts/discourse/helpers/test-plugin.js.es6"
 register_asset "javascripts/discourse/initializers/test-plugin.js.es6"
-register_asset "javascripts/discourse/templates/connectors/user-card-post-names/test-plugin.js.es6"
+register_asset "javascripts/discourse/templates/connectors/user-card-post-names/test-plugin.hbs"
 
 =begin
 
